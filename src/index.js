5934204820fadfd71d884043709e92d7556c5d2e
MATIAS GONZALEZ <matias.goonzalez@gmail.com>
2021-10-28 09:50:18 -0300

    create context

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { AppProvider } from './context/AppContext';
 
 import App from './App';
 
 
 ReactDOM.render(
+  <AppProvider>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </AppProvider>,
   document.getElementById('root')
 );
 
